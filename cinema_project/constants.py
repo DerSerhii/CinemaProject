d47749bfb37b5a8a93b834cbfe0b84d60d9ac410
serhii <proektssd@gmail.com>
2023-11-30 08:54:07 +0200

    feat(constants.py): added module with constants

---
+++ cinema_project/constants.py
@@ -0,0 +1,10 @@
+"""
+The module represents CONSTANTS for use in the project.
+"""
+
+# Dictionary keys for cleaned form data
+FILM = 'film'
+SHOWTIME = 'showtime'
+START_DATETIME = 'start_datetime'
+LAST_DAY = 'last_day'
+SCREEN = 'screen'
